c91a2add5bba4314e666b252d3b8e74182f5328d
Manoj Narasareddy <mn9353780784@gmail.com>
2026-05-29 02:11:59 +0530

    Fix: enable stable provideZonelessChangeDetection in appConfig to trigger change detection on event listeners

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners()],
+  providers: [
+    provideZonelessChangeDetection(),
+    provideBrowserGlobalErrorListeners()
+  ],
 };
